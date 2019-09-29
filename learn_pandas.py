d591ecb451b62228d583874c77403b2bb96d3f7e
shzy <zhangyueyork@outlook.com>
2019-09-29 18:27:17 +0800

    x

--- learn_pandas.py
+++ learn_pandas.py
@@ -2,7 +2,13 @@
 #
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-plt.style.use(["classic", "myself"])
+#import matplotlib.pyplot as plt
+#plt.style.use(["classic", "myself"])
 print("-"*10,"import end","-"*10)
 ########
+lst = [
+        [1,2,3,4],
+        [4,5,6,7],
+        [7,8,9,10]
+        ]
+df1 = pd.DataFrame(lst, index=[1,2,3], columns=list('ABCD'))
