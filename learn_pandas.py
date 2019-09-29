84d90a2fa57388d7b412858f1a079fd9829ca83b
shzy <zhangyueyork@outlook.com>
2019-09-29 10:56:52 +0800

    add file

---
+++ learn_pandas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use(["classic", "myself"])
+print("-"*10,"import end","-"*10)
+########
