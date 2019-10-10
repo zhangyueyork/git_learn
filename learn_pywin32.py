d974cfd959ca2f183fa230117f405ad9434325ad
shzy <zhangyueyork@outlook.com>
2019-10-10 10:51:34 +0800

    pywin32

---
+++ learn_pywin32.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+#
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#plt.style.use(["classic", "myself"])
+from win32com import client
+print("-"*10,"import end","-"*10)
+########
+docpath = '/Users/shzy/Desktop/code/shiyandoc.doc'
